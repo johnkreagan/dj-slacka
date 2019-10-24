13466e0752d7da9c335526b3df47ff2eb64eedd7
John Reagan <john.reagan@cstrauss.backstop.local>
2019-10-24 12:14:58 -0500

    Add name to api response

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, unique=True, primary_key=True)
-  name = db.Column(db.String(256), unique=True)
+  name = db.Column(db.String(256), unique=False)
   spotify_id = db.Column(db.String(64), unique=True)
   oauth = db.Column(db.String(256), unique=True)
   refresh_tok = db.Column(db.String(256), unique=True)
@@ -20,4 +20,4 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
   class Meta:
-    fields = ('spotify_id', 'id')
+    fields = ('spotify_id', 'id', 'name')
