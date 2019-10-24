4882aae98c242b9a1f64d651296c73659d4bbc4b
askowronski <askowronski@backstopsolutions.com>
2019-10-24 15:13:12 -0500

    DJSlacka correcting method imp

--- app.py
+++ app.py
@@ -95,6 +95,10 @@ def handle_event(event):
         return __spibot__.send_currently_playing_list(channel, get_tunes(membersInChannel, filterUsers))
     elif "help" in event_text:
         return __spibot__.send_data_to_slack(channel, get_help_text(), "Help Message Sent")
+    elif "delete" in event_text:
+        u = User.query.filter_by(slack_user_name=username).first()
+        db.session.add(u_mapping)
+        db.session.commit()
     else:
         return requests.make_response("invalid event", 500)
 
