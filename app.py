713ff56620500ba915725b1ae449be553de8c0fa
John Reagan <john.reagan@cstrauss.backstop.local>
2019-10-24 19:24:49 -0500

    Add logging

--- app.py
+++ app.py
@@ -171,9 +171,11 @@ def get_tunes_detailed():
     for user in User.query.all():
         try:
             track = __spibot__.get_currently_playing(user.oauth)
+            app.logger.error("track retrieved: %s added to db", track)
             if track:
                 track = track['item']
                 track_id = add_to_playlist(track, user, get_artists_string(track))
+                app.logger.error("track retrieved: %s added to db", track_id)
                 songs.append({"user":user.name,"track":track,"track_id":track_id})
 
         except SpotifyAuthTokenError:
