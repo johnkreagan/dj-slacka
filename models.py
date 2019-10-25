96ebe08b876b26d3f20a0e3ec686f319080763f4
John Reagan <john.reagan@JREAGAN1.local>
2019-10-25 16:20:56 -0500

    Fix the date stuff?

--- models.py
+++ models.py
@@ -81,12 +81,11 @@ class PlayedTracks(db.Model):
   id = db.Column(db.Integer, unique=True, primary_key=True)
   track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'),nullable=False)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
-  timestamp = db.Column(db.DateTime)
+  timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
   def __init__(self, track_id, user_id):
     self.track_id = track_id
     self.user_id = user_id
-    self.timestamp = datetime.datetime.utcnow
 
   def __repre__(self):
     return 'track played track_id : {}, user_id: {}, timestamp:{} >'.format(self.track_id, self.user_id, self.timestamp)
@@ -96,11 +95,10 @@ class LikedTracks(db.Model):
 
   id = db.Column(db.Integer, unique=True, primary_key=True)
   track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'),nullable=False)
-  timestamp = db.Column(db.DateTime)
+  timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
   def __init__(self, track_id):
     self.track_id = track_id
-    self.timestamp = datetime.datetime.utcnow
 
   def __repre__(self):
     return 'track like track_id :{}, timestamp:{} >'.format(self.track_id, self.timestamp)
